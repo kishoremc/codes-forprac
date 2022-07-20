75a99e5f5f7a4a354cf0d256c6873a90cdae1e06
kishore <kishore.gmail.com>
2022-07-20 16:31:34 +0000

    fact commit

--- fact.c
+++ fact.c
@@ -12,7 +12,7 @@ void factorial() {
         for (i = 1; i <= n; ++i) {
             fact *= i;
         }
-        printf("factorial of %d = %llu\n", n, fact);
+        printf("Factorial of %d = %llu\n", n, fact);
     }
 
     return 0;
